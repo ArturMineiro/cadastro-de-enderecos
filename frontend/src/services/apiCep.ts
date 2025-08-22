5d816ea65a2d89971d067fd579b6514a2590552b
artur.mineiro <arturmineiro2@hotmail.com>
2025-08-22 08:59:39 -0300

    implementando types e servies para rota via axios

---
+++ frontend/src/services/apiCep.ts
@@ -0,0 +1,11 @@
+// src/services/apiCep.ts
+import axios from 'axios';
+
+const apiCep = axios.create({
+  baseURL: 'http://localhost:8080/api/cep',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default apiCep;
---
+++ frontend/src/types/Endereco.ts
@@ -0,0 +1,13 @@
+// src/types/Endereco.ts
+export interface Endereco {
+  id: number;
+  nome: string;
+  cpf: string;
+  cep: string;
+  logradouro: string;
+  bairro: string;
+  cidade: string;
+  estado: string;
+  dataCriacao?: string;
+  dataAtualizacao?: string;
+}
